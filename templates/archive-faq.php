3e64b7b931b5afd50cf69427fb9017ebf52a974f
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-06-25 16:19:44 +0200

    5.3.51 (closes #176 )

--- templates/archive-faq.php
+++ templates/archive-faq.php
@@ -9,23 +9,27 @@
 */
 
 get_header();
-
 ?>
 
 <main id="main" class="site-main rrze-faq archive">
+    <div id="content"><div class="content-container">
+        <h2>FAQ</h2>
 
-<?php
-
-
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-
-include('template-parts/faq_content.php');
-
-endwhile; endif;
-
-?>
+        <ul>
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                printf(
+                    '<li><a href="%s">%s</a></li>',
+                    esc_url(get_the_permalink()),
+                    esc_html(get_the_title())
+                );
+            }
+        }
+        ?>
+        </ul>
+    </div></div>
 </main>
 
-<?php
-get_footer();
-
+<?php get_footer(); ?>
