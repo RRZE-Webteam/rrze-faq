3a60db7093af03dd05b530fc1f7678cdf2720aa2
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-08-22 13:06:30 +0200

    5.4.30 (fixes "GROUP GLOSSARY CONTENT BY" in Block editor)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '17574551b31e9d306f20');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '08dede0d9df234ffb94e');
